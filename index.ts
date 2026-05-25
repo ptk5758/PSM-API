0929d3d70196ee72a4669b351ce8773c855fcb87
ptk5758 <ptk725739@gmail.com>
2026-05-25 23:06:49 +0900

    [feature] add story api 초안

--- index.ts
+++ index.ts
@@ -2,6 +2,13 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json()) // application/json Body 로 파싱
+// app.use(express.urlencoded({extended: true})) // application/x-www-form-urlencoded 파싱
+
+app.post('/api/v1/story', async (request, response) => {
+    console.log(request.body)
+    response.status(200).json({ message: 'success' })
+})
 
 app.listen(8000, () => {
     console.log(`PORT 8000 Open`)
